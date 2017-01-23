9ce830fa77d35f0a7d39d5bc385ef55ffed3a535
camendola <chia.amendola@gmail.com>
2017-01-23 16:11:28 +0100

    update

--- EvalRate.cpp
+++ EvalRate.cpp
@@ -1,4 +1,5 @@
-// evaluates rate  - compile with c++ -lm -o EvalRate_OnL1Legacy EvalRate_OnL1Legacy.cpp `root-config --glibs --cflags`
+
+// evaluates rate  - compile with c++ -lm -o EvalRate EvalRate.cpp `root-config --glibs --cflags`
 // and launch the executable 
 
 #include <iostream>
@@ -12,91 +13,101 @@
 
 using namespace std;
 
-void EvalRate(){
+int main(){
   //double scaleRunI = 1./1.713;
   double scaleRunI = 1.075/1.713;
-  bool doScaleRunI = true;
+  bool doScaleRunI = false;
   
   cout << "Do scale: " << doScaleRunI << endl;
   cout << "Scale factor: " << scaleRunI << endl;
-  
+  TString directory = "/data_CMS/cms/amendola/RateStudiesL1Ntuples/Ntuples22Gen2016/";
   //    ZeroBias sample L1
-  TFile *file = TFile::Open("test.root","read");
+  TFile *file = TFile::Open(Form("%sData_L1ntuple_ZeroBias.root", directory.Data()),"read");
   TTree * tInput = (TTree*) file->Get("L1Tree/L1Tree");
-  
-    TString fOutName = "rateL1_test_";
-    
-    if (doScaleRunI) fOutName += "Scaled.root";
-    else fOutName += "NotScaled.root";
-    cout<<"check"<<endl;    
-
-
-    //  Int_t stage2objNumber; //0 = taus, 1 = eg, 2 = jets, 3 = muons  
+  if (file != NULL) cout<<"daje"<<endl;  
+  TString fOutName = "rateL1_";
+  fOutName = directory+fOutName;
+  if (doScaleRunI) fOutName += "Scaled.root";
+  else fOutName += "NotScaled.root";
 
   
-  Int_t stage2_tauN;
-  std::vector<Float_t> stage2_tauEt;
-  std::vector<Float_t> stage2_tauEta;
-  std::vector<Float_t> stage2_tauPhi;
+  
+  //  Int_t stage2objNumber; //0 = taus, 1 = eg, 2 = jets, 3 = muons  
 
   
-  Int_t stage2_jetN;
-  std::vector<Float_t> stage2_jetEt;
-  std::vector<Float_t> stage2_jetEta;
-  std::vector<Float_t> stage2_jetPhi;
+  Int_t stage2_tauN ;
+  std::vector<Float_t> *stage2_tauEt=0;
+  std::vector<Float_t> *stage2_tauEta=0;
+  std::vector<Float_t> *stage2_tauPhi=0;
 
+  
+  Int_t stage2_jetN ;
+  std::vector<Float_t> *stage2_jetEt=0;
+  std::vector<Float_t> *stage2_jetEta=0;
+  std::vector<Float_t> *stage2_jetPhi=0;
+  
   // set branch and variables
-      cout<<"declared"<<endl;    
+  TBranch  *b_stage2_tauN ;
+  TBranch *b_stage2_tauEt;
+  TBranch *b_stage2_tauEta;
+  TBranch *b_stage2_tauPhi;
   
-  tInput ->SetBranchAddress ("stage2_tauN", &stage2_tauN);
-  tInput ->SetBranchAddress ("stage2_tauEta", &stage2_tauEta);
-  tInput ->SetBranchAddress ("stage2_tauPhi", &stage2_tauPhi);
-  tInput ->SetBranchAddress ("stage2_tauPhi", &stage2_tauPhi);
-  tInput ->SetBranchAddress ("stage2_jetN", &stage2_jetN);
-  tInput ->SetBranchAddress ("stage2_jetEta", &stage2_jetEta);
-  tInput ->SetBranchAddress ("stage2_jetPhi", &stage2_jetPhi);
-  tInput ->SetBranchAddress ("stage2_jetPhi", &stage2_jetPhi);
-       cout<<"addressed"<<endl;    
+  TBranch *b_stage2_jetN ;
+  TBranch *b_stage2_jetEt;
+  TBranch *b_stage2_jetEta;
+  TBranch *b_stage2_jetPhi;
+  
+  
+  tInput ->SetBranchAddress("stage2_tauN", &stage2_tauN , &b_stage2_tauN );
+  tInput ->SetBranchAddress("stage2_tauEta", &stage2_tauEta, &b_stage2_tauEta);
+  tInput ->SetBranchAddress("stage2_tauPhi", &stage2_tauPhi, &b_stage2_tauPhi);
+  tInput ->SetBranchAddress("stage2_tauEt", &stage2_tauEt, &b_stage2_tauEt);
+  tInput ->SetBranchAddress("stage2_jetN", &stage2_jetN , &b_stage2_jetN);
+  tInput ->SetBranchAddress("stage2_jetEta", &stage2_jetEta, &b_stage2_jetEta);
+  tInput ->SetBranchAddress("stage2_jetPhi", &stage2_jetPhi, &b_stage2_jetPhi);
+  tInput ->SetBranchAddress("stage2_jetEt", &stage2_jetEt, &b_stage2_jetEt);
+  
 
-       tInput->Show();  
-
-    ///////////////
-    //// HISTO ////
-    ///////////////
-
-    TFile* fOut = new TFile (fOutName, "recreate");
-   
-    // the tree histograms are filed in identical way, structure is simply copied from the Stage 2 rate Evaluator
-    TH1D* LeadPt_passIso = new TH1D ("LeadPt_passIso", "LeadPt_passIso", 5000, 0, 5000);
-    TH1D* SubleadPt_passIso = new TH1D ("SubleadPt_passIso", "SubleadPt_passIso", 5000, 0, 5000);
-    TH1D* RelativeRate_singleTau = new TH1D ("RelativeRate_singleTau", "Relative rate - single tau", 5000, 0, 5000);
-    TH1D* RelativeRate_diTau = new TH1D ("RelativeRate_diTau", "Relative rate - double tau", 5000, 0, 5000);
-
-    TH1D* LeadPt_NoIso = new TH1D ("LeadPt_NoIso", "LeadPt_NoIso", 5000, 0, 5000);
-    TH1D* SubleadPt_NoIso = new TH1D ("SubleadPt_NoIso", "SubleadPt_NoIso", 5000, 0, 5000);
-    TH1D* RelativeRate_singleTau_NoIso = new TH1D ("RelativeRate_singleTau_NoIso", "Relative rate - single tau", 5000, 0, 5000);
-    TH1D* RelativeRate_diTau_NoIso = new TH1D ("RelativeRate_diTau_NoIso", "Relative rate - double tau", 5000, 0, 5000);
-
-    TH1D* LeadPt_IsoAndShape = new TH1D ("LeadPt_IsoAndShape", "LeadPt_IsoAndShape", 5000, 0, 5000);
-    TH1D* SubleadPt_IsoAndShape = new TH1D ("SubleadPt_IsoAndShape", "SubleadPt_IsoAndShape", 5000, 0, 5000);
-    TH1D* RelativeRate_singleTau_IsoAndShape = new TH1D ("RelativeRate_singleTau_IsoAndShape", "Relative rate - single tau", 5000, 0, 5000);
-    TH1D* RelativeRate_diTau_IsoAndShape = new TH1D ("RelativeRate_diTau_IsoAndShape", "Relative rate - double tau", 5000, 0, 5000);
-
-        cout<<"histodeclared"<<endl;    
-    // analyze data    
-    long int nEvents = tInput->GetEntries(); 
+  tInput->Show();  
+  
+  ///////////////
+  //// HISTO ////
+  ///////////////
+  
+  TFile* fOut = new TFile (Form("%s",fOutName.Data()), "recreate");
+  if (fOut!=NULL) cout<<"dajetutti"<<endl; 
+  // the tree histograms are filed in identical way, structure is simply copied from the Stage 2 rate Evaluator
+  TH1D* LeadPt_passIso = new TH1D ("LeadPt_passIso", "LeadPt_passIso", 5000, 0, 5000);
+  TH1D* SubleadPt_passIso = new TH1D ("SubleadPt_passIso", "SubleadPt_passIso", 5000, 0, 5000);
+  TH1D* RelativeRate_singleTau = new TH1D ("RelativeRate_singleTau", "Relative rate - single tau", 5000, 0, 5000);
+  TH1D* RelativeRate_diTau = new TH1D ("RelativeRate_diTau", "Relative rate - double tau", 5000, 0, 5000);
+  
+  TH1D* LeadPt_NoIso = new TH1D ("LeadPt_NoIso", "LeadPt_NoIso", 5000, 0, 5000);
+  TH1D* SubleadPt_NoIso = new TH1D ("SubleadPt_NoIso", "SubleadPt_NoIso", 5000, 0, 5000);
+  TH1D* RelativeRate_singleTau_NoIso = new TH1D ("RelativeRate_singleTau_NoIso", "Relative rate - single tau", 5000, 0, 5000);
+  TH1D* RelativeRate_diTau_NoIso = new TH1D ("RelativeRate_diTau_NoIso", "Relative rate - double tau", 5000, 0, 5000);
+  
+  TH1D* LeadPt_IsoAndShape = new TH1D ("LeadPt_IsoAndShape", "LeadPt_IsoAndShape", 5000, 0, 5000);
+  TH1D* SubleadPt_IsoAndShape = new TH1D ("SubleadPt_IsoAndShape", "SubleadPt_IsoAndShape", 5000, 0, 5000);
+  TH1D* RelativeRate_singleTau_IsoAndShape = new TH1D ("RelativeRate_singleTau_IsoAndShape", "Relative rate - single tau", 5000, 0, 5000);
+  TH1D* RelativeRate_diTau_IsoAndShape = new TH1D ("RelativeRate_diTau_IsoAndShape", "Relative rate - double tau", 5000, 0, 5000);
+  
+  
+  // analyze data    
+    int nEvents = tInput->GetEntries(); 
     //long int nEvents = 100000; 
-    
+    cout<<nEvents<<" nEvents"<<endl;
     std::vector<double> pt_pass; // just pass iso
     std::vector<double> pt_passAndShape; // also pass veto shape
     std::vector<double> pt_noiso;
-      cout<<"vectorsdeclared"<<endl;    
+    
     // loop on all events
-    for (long int iEv = 0; iEv < nEvents; iEv++)
+    for (int iEv = 0; iEv < nEvents; iEv++)
     {
+
         tInput->GetEntry(iEv);
-  cout<<"loop get entry"<<endl;         
-   if (iEv%100000 == 0) cout << iEv << " / " << nEvents << endl;
+
+   if (iEv%1000 == 0) cout << iEv << " / " << nEvents << endl;
         
         // clear pt vector for this event and compute number of TT
         pt_pass.clear();
@@ -109,8 +120,8 @@ void EvalRate(){
         for (int iL1 = 0; iL1 < stage2_tauN; iL1++)
         {
             // selections
-            double abseta = TMath::Abs( (stage2_tauEta)[iL1] );
-            double tauPt  = (stage2_tauPhi)[iL1];
+            double abseta = TMath::Abs( (*stage2_tauEta)[iL1] );
+            double tauPt  = (*stage2_tauPhi)[iL1];
             
             if (doScaleRunI) tauPt = scaleRunI*tauPt;
 
