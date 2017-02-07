08b85457a0b35970776cb6ddb3885f01afd83ba8
camendola <chia.amendola@gmail.com>
2017-02-07 16:19:10 +0100

    VBF trigger rate

--- EvalRate.cpp
+++ EvalRate.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include "TCanvas.h"
+#include "TLorentzVector.h"
 #include "TH1D.h"
 #include "TH2D.h"
 #include "TTree.h"
@@ -14,22 +15,81 @@
 
 using namespace std;
 
-int main(int argc, char** argv){
-  //double scaleRunI = 1./1.713;
-  double scaleRunI = 1.075/1.713;
-  bool doScaleRunI = false;
+class object{
+public:
+  object(float Et, float Eta, float Phi){Et_=Et; Eta_ = Eta;Phi_ = Phi;};
+  float Et() const{
+    return Et_;
+  }
+  float Phi() const {
+    return Phi_;
+  }
+  float Eta() const {
+    return Eta_;
+  }
   
-  cout << "Do scale: " << doScaleRunI << endl;
-  cout << "Scale factor: " << scaleRunI << endl;
-  TString directory = "/data_CMS/cms/amendola/RateStudiesL1Ntuples/L1NtuplesOutput_ZeroBias25Gen2016HighPU_ibx0_100000Events/";
+  class SortByDeltaR{
+  public:    
+    SortByDeltaR(float ref_Phi, float ref_Eta){ref_Phi_ = ref_Phi; ref_Eta_= ref_Eta;};
+    bool operator() (const object& obj1,const object& obj2)
+    {
+      float dEta1 = obj1.Eta()-ref_Eta_;
+      
+      float dPhi1 = obj1.Phi()-ref_Phi_;
+      if(dPhi1 > 3.14) dPhi1 -= 6.28;
+      if(dPhi1 < -3.14) dPhi1 += 6.28;
+      
+      float dEta2 = obj2.Eta()-ref_Eta_;
+      
+      float dPhi2 = obj2.Phi()-ref_Phi_;
+      if(dPhi2 > 3.14) dPhi2 -= 6.28;
+      if(dPhi2 < -3.14) dPhi2 += 6.28;
+      return sqrt(dEta1*dEta1 + dPhi1*dPhi1) < sqrt(dEta2*dEta2 + dPhi2*dPhi2);
+    }
+  private: 
+    float ref_Phi_, ref_Eta_;  
+    
+  }; 
+  
+  bool inline operator <(const object& objref) const {
+    return this->Et() > objref.Et();
+  }
+  
+  
+  friend std::ostream& operator << (std::ostream &out, const object &object);
+private:
+  float Et_, Eta_, Phi_;
+};
+
+
+std::ostream & operator << (std::ostream &out, const object &object)
+{
+  out<<object.Et();
+  return out;
+}
+
+
+int main(int argc, char** argv){
+
+  float nbStudiedRun = 96;
+  float thisLumiRun = 7.6E32;
+  float scaleToLumi = 1.8E34;
+  float scale = 0.001*(nbStudiedRun*11245.6)*scaleToLumi/thisLumiRun;  
+
+  
+
+  cout << "Scale factor: " << scale << endl;
+  // TString directory = "/data_CMS/cms/amendola/RateStudiesL1Ntuples/L1NtuplesOutput_ZeroBias25Gen2016HighPU_ibx0_100000Events/";
+  TString directory = "/data_CMS/cms/amendola/RateStudiesL1Ntuples/L1NtuplesOutput_ZeroBias6Feb2017HighPU_ibx0_-1Events/";
+
   //ZeroBias sample L1
-  TFile *file = TFile::Open(Form("%sL1ntuples_0.root", directory.Data()),"read");
+  TFile *file = TFile::Open(Form("%sL1total.root", directory.Data()),"read");
   TTree * tInput = (TTree*) file->Get("L1Tree/L1Tree");
   
-  TString fOutName = "rateL1_";
+  TString fOutName = "rateL1_VBF";
   fOutName = directory+fOutName;
-  if (doScaleRunI) fOutName += "Scaled.root";
-  else fOutName += "NotScaled.root";
+  fOutName += ".root";
+
     
   
   Int_t stage2_tauN ;
@@ -71,26 +131,23 @@ int main(int argc, char** argv){
   
   TFile* fOut = new TFile (Form("%s",fOutName.Data()), "recreate");
   
-  // the tree histograms are filed in identical way, structure is simply copied from the Stage 2 rate Evaluator
+ 
   TH1D* LeadTauPt_Pass = new TH1D ("LeadTauPt_Pass", "LeadTauPt_Pass", 50, 0, 100);
   TH1D* SingleTauPt_Pass = new TH1D ("SingleTauPt_Pass", "SingleTauPt_Pass", 50, 0, 100);
   TH1D* SubleadTauPt_Pass = new TH1D ("SubleadTauPt_Pass", "SubleadTauPt_Pass", 50, 0, 100);
   TH1D* DiTauPt_Pass = new TH1D ("DiTauPt_Pass", "DiTauPt_Pass", 50, 0, 100);
-  TH2D* DiTau2D_Pass = new TH2D ("DiTau2D_Pass", "DiTau2D_Pass", 50, 0, 100, 50, 0 , 100);
   TH1D* DiTauSingleJetPt_Pass = new TH1D ("DiTauSingleJetPt_Pass", "DiTauSingleJetPt_Pass", 50, 0, 100);
-  TH2D* DiTauSingleJet2D_Pass = new TH2D ("DiTauSingleJet2D", "single tau plus jet", 50, 0, 100, 10, 0, 5);
-  TH2D* SingleTauSingleJet2D_Pass = new TH2D ("SingleTauSingleJet2D", "double tau const", 50, 0, 100, 10, 0, 5);
+  TH2D* DiJet2D_Pass = new TH2D ("DiJet2D", "double jet const", 100, 0, 800, 100,30, 130);
 
   TH1D* RelativeRate_singleTau = new TH1D ("RelativeRate_singleTau", "Relative rate - single tau", 50, 0, 100);
   TH1D* RelativeRate_diTau = new TH1D ("RelativeRate_diTau", "Relative rate - double tau", 50, 0, 100);
   TH1D* RelativeRate_diTauSublead = new TH1D ("RelativeRate_diTauSublead", "Relative rate - double tau", 50, 0, 100);
-  TH2D* RelativeRate_diTau2D = new TH2D ("RelativeRate_diTau2D", "Relative rate - double tau", 50, 0, 100, 50, 0 ,100);
   TH1D* RelativeRate_diTauSingleJet = new TH1D ("RelativeRate_diTauSingleJet", "Relative rate - double tau plus jet", 50, 0, 100);
-  TH1D* RelativeRate_diTauRef = new TH1D ("RelativeRate_diTauRef", "Relative rate - double tau const", 50, 0, 100);
-  TH2D* RelativeRate_diTauSingleJet2D = new TH2D ("RelativeRate_diTauSingleJet2D", "Relative rate - single tau plus jet", 50, 0, 100, 50, 0, 100);
-  TH2D* RelativeRate_SingleTauSingleJet2D = new TH2D ("RelativeRate_SingleTauSingleJet2D", "Relative rate - double tau const", 50, 0, 100, 50, 0, 100);
-
-  
+  TH2D* RelativeRate_SingleTauSingleJet2D = new TH2D ("RelativeRate_SingleTauSingleJet2D", "Relative rate - double tau ", 50, 0, 100, 50, 0, 100);
+ 
+  //VBF
+  TH2D* Ratio_DiJet2D = new TH2D ("Ratio_DiJet2D", "Ratio - 2 jets, E_{T} > 30 GeV ", 100, 0, 800, 100, 30, 130);
+  TH2D* Rate_DiJet2D = new TH2D ("Rate_DiJet2D", "Rate - 2 jets, E_{T} > 30 GeV ", 100, 0, 800, 100, 30, 130); 
 
   
   
@@ -98,6 +155,8 @@ int main(int argc, char** argv){
   long int nEvents = tInput->GetEntries(); 
   std::vector<double> ptTau_pass; 
   std::vector<double> ptJet_pass;
+
+  std::vector<double> mjj_pass; //VBF
   double singletau = 0;
   double ditau = 0;  
   // loop on all events
@@ -105,59 +164,81 @@ int main(int argc, char** argv){
     
     tInput->GetEntry(iEv);
     
-    if (iEv%1000 == 0) cout << iEv << " / " << nEvents << endl;
+     if (iEv%1000 == 0) cout << iEv << " / " << nEvents << endl;
+     std::vector<object> jet30;   
+    //std::vector<object> tau;
+    //vector <float> DeltaRmin_taujet; //needed to reject tau-jet overlap
+    //vector <float> DeltaRmin_jetjet; //needed to reject jet-jet overlap    
+     ptTau_pass.clear();
+     ptJet_pass.clear();
+     mjj_pass.clear();
+     
+     for (long int iL1 = 0; iL1 < stage2_tauN; iL1++){
+       // selections
+       double tauPt  = stage2_tauEt->at(iL1);
+       ptTau_pass.push_back (tauPt);
+     }
     
-    // clear pt vector for this event 
-    ptTau_pass.clear();
-    ptJet_pass.clear();
+     for (long int iL1 = 0; iL1 < stage2_jetN; iL1++){
+       // selections
+       double jetPt  = (*stage2_jetEt)[iL1];
+       ptJet_pass.push_back (jetPt);
+       if(jetPt>30.)	jet30.push_back(object(stage2_jetEt->at(iL1),stage2_jetEta->at(iL1),stage2_jetPhi->at(iL1))) ;//VBF 
+     }
+
+     std::sort (ptTau_pass.begin(), ptTau_pass.end());
+     std::sort (ptJet_pass.begin(), ptJet_pass.end());
     
-    
-    for (long int iL1 = 0; iL1 < stage2_tauN; iL1++){
-      // selections
-      double abseta = TMath::Abs(stage2_tauEta->at(iL1) );
-      double tauPt  = stage2_tauEt->at(iL1);
-      
-      if (doScaleRunI) tauPt = scaleRunI*tauPt;
-      
-      if (abseta < 2.1){	      
-	ptTau_pass.push_back (tauPt);
+     //VBF    
+     std::sort (jet30.begin(),jet30.end());
+     int iJet = 0;
+     int kJet = 0;
+    if (jet30.size() >= 2){
+      for (iJet= 0; iJet <jet30.size(); iJet++){      
+	for (kJet= 0; kJet <jet30.size(); kJet++){      
+	  if (kJet!=iJet) {
+	    
+	    TLorentzVector ijet;
+	    ijet.SetPtEtaPhiM(
+			      jet30[iJet].Et(),
+			      jet30[iJet].Eta(),
+			      jet30[iJet].Phi(),
+			      0.);
+	    TLorentzVector kjet;
+	    kjet.SetPtEtaPhiM(
+			      jet30[kJet].Et(),
+			      jet30[kJet].Eta(),
+			      jet30[kJet].Phi(),
+			      0.);
+	    TLorentzVector jetPair = ijet+kjet;
+	    mjj_pass.push_back(jetPair.M());
+
+	  }
+
+	}
+
       }
+      std::sort(mjj_pass.begin(),mjj_pass.end());
+      DiJet2D_Pass -> Fill ( *(mjj_pass.rbegin()),jet30[0].Et());
+    } else {
+      DiJet2D_Pass -> Fill (-1, -1);
     }
-    
-    for (long int iL1 = 0; iL1 < stage2_jetN; iL1++){
-      // selections
-      double abseta = TMath::Abs( (*stage2_jetEta)[iL1] );
-      double jetPt  = (*stage2_jetPhi)[iL1];
-      
-      if (doScaleRunI) jetPt = scaleRunI*jetPt;
-      
-      if (abseta < 2.4){
-	ptJet_pass.push_back (jetPt);
-      }
-    }
-    
-    
-    // now that all taus in the event are analyzed, fill the histograms of lead and sublead pt (if missing, fill with a -1)
-    std::sort (ptTau_pass.begin(), ptTau_pass.end());
-    std::sort (ptJet_pass.begin(), ptJet_pass.end());
+
     
     
     if (ptTau_pass.size() >= 2 )
       {
 	ditau += 1;
 	singletau += 1;
-	    LeadTauPt_Pass -> Fill ( *(ptTau_pass.rbegin()) ); 
-	    SingleTauPt_Pass -> Fill ( *(ptTau_pass.rbegin()) ); 
-	    SubleadTauPt_Pass -> Fill ( *(ptTau_pass.rbegin() + 1) );
-	    DiTauPt_Pass -> Fill (*(ptTau_pass.rbegin()) +*(ptTau_pass.rbegin() + 1) );
-	    DiTau2D_Pass -> Fill (*(ptTau_pass.rbegin()),*(ptTau_pass.rbegin() + 1) );
-	    if (ptJet_pass.size()>=1){
-	      DiTauSingleJetPt_Pass->Fill( *(ptJet_pass.rbegin()) );
-	      DiTauSingleJet2D_Pass->Fill( *(ptJet_pass.rbegin()), *(ptTau_pass.rbegin()) +*(ptTau_pass.rbegin() + 1) );
-	    }else{
-	      DiTauSingleJetPt_Pass->Fill( -1 );
-	      DiTauSingleJet2D_Pass->Fill( -1,-1 );
-	    }
+	LeadTauPt_Pass -> Fill ( *(ptTau_pass.rbegin()) ); 
+	SingleTauPt_Pass -> Fill ( *(ptTau_pass.rbegin()) ); 
+	SubleadTauPt_Pass -> Fill ( *(ptTau_pass.rbegin() + 1) );
+	DiTauPt_Pass -> Fill (*(ptTau_pass.rbegin()) +*(ptTau_pass.rbegin() + 1) );
+	if (ptJet_pass.size()>=1){
+	  DiTauSingleJetPt_Pass->Fill( *(ptJet_pass.rbegin()) );
+	}else{
+	  DiTauSingleJetPt_Pass->Fill( -1 );
+	}
       } 
     
     else if (ptTau_pass.size() == 1){
@@ -167,12 +248,6 @@ int main(int argc, char** argv){
       SubleadTauPt_Pass -> Fill ( -1 );
       DiTauPt_Pass -> Fill ( -1); 
       DiTauSingleJetPt_Pass->Fill( -1 );
-      DiTau2D_Pass->Fill(-1,-1);
-      if (ptJet_pass.size()>=1){
-	SingleTauSingleJet2D_Pass->Fill( *(ptJet_pass.rbegin()), *(ptTau_pass.rbegin()) );
-      }else{
-	SingleTauSingleJet2D_Pass->Fill( -1,-1 );
-      }       
     } 
     
     else
@@ -182,49 +257,53 @@ int main(int argc, char** argv){
 	SubleadTauPt_Pass -> Fill ( -1 );        
 	DiTauPt_Pass -> Fill ( -1 );        
 	DiTauSingleJetPt_Pass->Fill( -1 );
-	DiTauSingleJet2D_Pass->Fill( -1,-1 );
-	SingleTauSingleJet2D_Pass->Fill( -1,-1 );
-	DiTau2D_Pass->Fill(-1,-1);
       }       
   }
   
   // compute rate plots
   cout << "Computing rates..." << endl; 
   
+
+
+  //taus
   for (int i = 1; i <= 50; i++){
-    // int Tot_singleTau = LeadTauPt_Pass -> Integral (0, 51);
+
     int Tot_diTauSublead = SubleadTauPt_Pass -> Integral (1,51);
     
     int Tot_singleTau = SingleTauPt_Pass -> Integral (1,51);
     int Tot_diTau = DiTauPt_Pass -> Integral (1,51);        
     int Tot_diTauSingleJet = DiTauSingleJetPt_Pass -> Integral (1,51);        
-    int Tot_diTauSingleJet2D = DiTauSingleJet2D_Pass -> Integral (1,51);       
-    int Tot_SingleTauSingleJet2D = SingleTauSingleJet2D_Pass -> Integral (1,51);        
-    int Tot_diTau2D = DiTau2D_Pass -> Integral (1,51);        
-    
-    //  double relRateSingleTau = 1.*(LeadTauPt_Pass->Integral(i, 51))/Tot_singleTau;
-    //  double relRateDiTau = 1.*(SubleadTauPt_Pass->Integral(i, 51))/Tot_diTau;
-    
+
     double relRateSingleTau = 1.*(SingleTauPt_Pass->Integral(i, 51))/Tot_singleTau;
     double relRateDiTau = 1.*(DiTauPt_Pass->Integral(i, 51))/Tot_diTau;
     double relRateDiTauSingleJet = 1.*(DiTauSingleJetPt_Pass->Integral(i, 51))/Tot_diTauSingleJet;
     double relRateDiTauSublead =  1.*(SubleadTauPt_Pass->Integral(i, 51))/Tot_diTauSublead;
+
     
     RelativeRate_singleTau -> SetBinContent (i, relRateSingleTau);
     RelativeRate_diTau -> SetBinContent (i, relRateDiTau);        
     RelativeRate_diTauSublead -> SetBinContent (i, relRateDiTauSublead);        
     
     RelativeRate_diTauSingleJet -> SetBinContent (i, relRateDiTauSingleJet);        
-    for (int j = 1; j<=50;j++ ){
-      double relRateDiTauSingleJet2D = 1.*(DiTauSingleJet2D_Pass->Integral(i, 51,j,51))/Tot_diTauSingleJet2D;
-      double relRateSingleTauSingleJet2D = 1.*(SingleTauSingleJet2D_Pass->Integral(i, 51,j,51))/Tot_SingleTauSingleJet2D;
-      double relRateDiTau2D = 1.*(DiTau2D_Pass->Integral(i, 51,j,51))/Tot_diTau2D;
-      RelativeRate_SingleTauSingleJet2D -> SetBinContent (i,j,relRateSingleTauSingleJet2D  );
-      RelativeRate_diTauSingleJet2D -> SetBinContent (i, j,relRateDiTauSingleJet2D);        
-      RelativeRate_diTau2D -> SetBinContent (i, j, relRateDiTau2D);        
-    }
+
   }
+
   cout<<"Fraction of events with at least 1 tau: "<<singletau/nEvents<<endl;
   cout<<"Fraction of events with at least 2 taus: "<<ditau/nEvents<<endl;   
+
+
+  
+  //VBF
+  for (int i = 1; i <=DiJet2D_Pass->GetNbinsX(); i++){
+    for (int j = 1; j<= DiJet2D_Pass->GetNbinsY();j++ ){
+      double binDiJet2D = 1.*(DiJet2D_Pass->Integral(i, DiJet2D_Pass->GetNbinsX()+1,j,DiJet2D_Pass->GetNbinsY()+1))/nEvents;
+      Ratio_DiJet2D -> SetBinContent (i, j, binDiJet2D);        
+      binDiJet2D *=scale;
+      Rate_DiJet2D -> SetBinContent (i, j, binDiJet2D);        
+      
+    }
+  }
+
+
   fOut -> Write();
 }
---
+++ makePlots.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python 
+from ROOT import *
+from array import *
+from math import fabs,sqrt
+
+DIR= '/data_CMS/cms/amendola/RateStudiesL1Ntuples/L1NtuplesOutput_ZeroBias6Feb2017HighPU_ibx0_-1Events/'
+FILEIN = 'L1total.root'
+FILEOUT = 'L1Plots.root'
+
+FILEIN = DIR+FILEIN
+FILEOUT = DIR+FILEOUT
+
+class Plots:
+	def __init__(self, name):
+		self.name = name
+		self.plots = {} 
+		
+	def addPlot(self, plotName, nbins, xmin, xmax,xlabel = '',ylabel = ''):
+		plot = TH1F ('plot_'+plotName+'_'+self.name, 'plot_'+plotName+'_'+self.name, nbins, xmin, xmax)
+		plot.Sumw2(True)       
+		plot.SetTitle('')
+		plot.GetXaxis().SetTitle(xlabel)
+		plot.GetYaxis().SetTitle(ylabel)       
+		self.plots[plotName] = plot
+  
+	def add2DPlot (self, plotName, xbins, xmin, xmax,ybins,ymin,ymax,xlabel = '',ylabel = ''):
+		plot2d = TH2F ('plot_'+name+'_'+self.name, 'plot_'+name+'_'+self.name, xbins, xmin, xmax, ybins, ymin,ymax)
+		plot2d.GetXaxis().SetTitle(xlabel)
+		plot2d.GetYaxis().SetTitle(ylabel)
+		plot2d.Sumw2(True) 
+		plot2d.Draw("colz")
+		self.plots[name] = plot2d
+		
+	def saveToFile(self, tFile):
+		tFile.cd()
+		for x in self.plots:
+			self.plots[x].Write()
+
+
+
+	def getPlot (self, name, isPass= False): # isPass: True: for _pass_, False for _tot_, 
+		return self.plots[name]
+
+
+
+class effPlots:
+	def __init__(self, name):
+		self.name = name
+		self.plots = {} 
+		self.effplots = {} ## contains efficiency plots
+       
+ 
+	def addPlot(self, plotName, nbins, xmin, xmax):
+		plot_pass = TH1F ('plot_pass_'+plotName+'_'+self.name, 'plot_pass_'+plotName+'_'+self.name, nbins, xmin, xmax)
+		plot_tot  = TH1F ('plot_tot_'+plotName+'_'+self.name, 'plot_tot_'+plotName+'_'+self.name, nbins, xmin, xmax)
+		self.plots[plotName] = [plot_pass, plot_tot]
+            ## call sumw2
+		for x in self.plots[plotName]:
+			x.Sumw2(True)
+
+	def add2DPlot (self, plotName, xbins, xmin, xmax,ybins,ymin,ymax):
+		plot_pass = TH2F ('plot_pass_'+plotName+'_'+self.name, 'plot_pass_'+plotName+'_'+self.name, xbins, xmin, xmax,ybins,ymin,ymax)
+		plot_tot  = TH2F ('plot_tot_'+plotName+'_'+self.name, 'plot_tot_'+plotName+'_'+self.name, xbins, xmin, xmax,ybins,ymin,ymax)
+		self.plots[plotName] = [plot_pass, plot_tot]
+            ## call sumw2
+		for x in self.plots[plotName]:
+			x.Sumw2(True)
+            
+            
+	def makeEffPlot(self, plotName,xlabel,ylabel):
+		# hwPass = self.plots[plotName][0].Clone(self.name+'_eff_'+plotName)
+		# hwTot = self.plots[plotName][2].Clone(self.name+'_tot_'+plotName)
+		# hwPass.Add(self.plots[plotName][1], -1)
+		# hwTot.Add(self.plots[plotName][3], -1)
+		# hwPass.Divide(hwTot)
+		# self.effplots[plotName] = hwPass
+		hwPass = self.plots[plotName][0].Clone(self.name+'_pass_'+plotName)
+		hwTot = self.plots[plotName][1].Clone(self.name+'_tot_'+plotName)
+		grEff = TGraphAsymmErrors()
+		grEff.SetName(self.name+'_'+plotName)
+		grEff.BayesDivide(hwPass, hwTot)
+		grEff.SetMarkerStyle(8)
+		grEff.GetXaxis().SetTitle(xlabel)
+		grEff.GetYaxis().SetTitle(ylabel)
+		self.effplots[plotName] = grEff
+
+
+	def make2DEffPlot(self, plotName,xlabel,ylabel):
+        # hwPass = self.plots[plotName][0].Clone(self.name+'_eff_'+plotName)
+        # hwTot = self.plots[plotName][2].Clone(self.name+'_tot_'+plotName)
+        # hwPass.Add(self.plots[plotName][1], -1)
+        # hwTot.Add(self.plots[plotName][3], -1)
+        # hwPass.Divide(hwTot)
+        # self.effplots[plotName] = hwPass
+		hEff = self.plots[plotName][0].Clone(self.name+'_pass_'+plotName)
+		hTot = self.plots[plotName][1].Clone(self.name+'_tot_'+plotName)
+		
+		hEff.SetName(self.name+'_'+plotName)
+		hEff.Divide(hTot)
+		
+		hEff.GetXaxis().SetTitle(xlabel)
+		hEff.GetYaxis().SetTitle(ylabel)
+		self.effplots[plotName] = hEff
+		
+
+
+   
+
+            
+	def saveToFile(self, tFile):
+		tFile.cd()
+		for x in self.plots:
+			for y in self.plots[x]:
+				y.Write()
+		for x in self.effplots:
+			self.effplots[x].Write()
+	    
+	    
+	def getPlot (self, name, isPass= False): # isPass: True: for _pass_, False for _tot_, 
+		if isPass:
+			return self.plots[name][0]
+		
+		if not isPass:
+			return self.plots[name][1]
+            
+	def getEffPlot (self, name): 
+		return self.effplots[name]
+    
+
+
+
+
+
+
+Plots = Plots("Plots")
+effPlots = effPlots("effPlots")
+
+
+Plots.addPlot('DeltaR_taujet',20,0,0.5,'#Delta R(L1#tau,L1jet)','Events')
+Plots.addPlot('DeltaR_jetjet',50,0,5,'#Delta R(L1jet_{i},L1jet_{k})','Events')
+
+
+
+#### 
+fIn   = TFile.Open(FILEIN)
+if not fIn.IsZombie(): print ('File '+FILEIN+' opened')
+tIn   = fIn.Get('L1Tree/L1Tree')
+nEvt  = tIn.GetEntries()
+
+
+if (nEvt > 100000): nEvt = 100000 
+
+for ev in range(0, nEvt):
+    tIn.GetEntry(ev)
+    if (ev%10000==0): print 'Entry {0} / {1}'.format(ev,nEvt) 
+    #variables
+
+    jetN = tIn.Stage2jetsNumber
+    tauN = tIn.Stage2tausNumber
+    mass = float(0.)        
+#facciamo una bella funzione per questa cosa?
+    dR_taujet = []
+    dR_jetjet = []
+    for iJet in range(0,jetN):
+
+        jetEt = tIn.stage2_jetEt.at(iJet)   
+        jetEta = tIn.stage2_jetEta.at(iJet)
+        jetPhi = tIn.stage2_jetPhi.at(iJet)
+        jet = TLorentzVector() 
+        jet.SetPtEtaPhiM(
+            jetEt,
+            jetEta,
+            jetPhi,
+            mass
+            )
+        for iTau in range(0,tauN):
+            tauEt = tIn.stage2_tauEt.at(iTau)   
+            tauEta = tIn.stage2_tauEta.at(iTau)
+            tauPhi = tIn.stage2_tauPhi.at(iTau)
+            tau = TLorentzVector() 
+            tau.SetPtEtaPhiM(
+                tauEt,
+                tauEta,
+                tauPhi,
+                mass
+                )
+            dR_taujet.append(tau.DeltaR(jet))
+	for kJet in range(0,jetN):
+		if not(kJet==iJet):
+			jet2Et = tIn.stage2_jetEt.at(kJet)   
+			jet2Eta = tIn.stage2_jetEta.at(kJet)
+			jet2Phi = tIn.stage2_jetPhi.at(kJet)
+			jet2 = TLorentzVector() 
+			jet2.SetPtEtaPhiM(
+				jet2Et,
+				jet2Eta,
+				jet2Phi,
+				mass
+				)
+			dR_jetjet.append(jet2.DeltaR(jet))
+    
+
+    dR_taujet = sorted(dR_taujet)
+    dR_jetjet = sorted(dR_jetjet)
+
+   
+#conditions
+    
+
+
+#fill plots        
+    if len(dR_taujet)>0: Plots.getPlot('DeltaR_taujet',False).Fill(dR_taujet[0])
+    if len(dR_jetjet)>0: Plots.getPlot('DeltaR_jetjet',False).Fill(dR_jetjet[0])
+    
+
+fIn.Close()
+
+
+
+
+
+fOut = TFile (FILEOUT, "recreate")
+
+Plots.saveToFile(fOut)
+if not fOut.IsZombie(): print ('Plots saved in '+FILEOUT)
+
+
+
+
+
+
+
+
+
